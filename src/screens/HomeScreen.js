070de42165fecbf254aacba9d41de626a7dadee2
sergio junca <sergio.junca@gmail.com>
2023-01-26 09:53:14 -0500

    chore: refactored screed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text, SafeAreaView, Image, Platform } from 'react-native';
+import {View, Text, SafeAreaView, Image, Platform} from 'react-native';
 
-import { StyleSheet } from 'react-native';
-import { Colors } from '../utils/Colors';
+import {StyleSheet} from 'react-native';
+import {Colors} from '../utils/Colors';
 
 import OpeningHeader from '../components/OpeningHeader';
 import ClosedDay from '../components/ClosedDay';
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     height: Platform.OS === 'ios' ? '75%' : '90%',
     borderRadius: 15,
     shadowColor: Colors.Grey3.color,
-    shadowOffset: { width: 0.5, height: 0.5 },
+    shadowOffset: {width: 0.5, height: 0.5},
     shadowOpacity: 0.9,
     shadowRadius: 5,
     elevation: 5,
@@ -67,12 +67,79 @@ const styles = StyleSheet.create({
   },
 });
 
-const HomeScreen = ({ }) => {
-  const { dataOpeningTimes, loadingOpeningTimes, errorOpeningTimesResponse } =
+const HomeScreen = ({}) => {
+  const {dataOpeningTimes, loadingOpeningTimes, errorOpeningTimesResponse} =
     useGetOpeningHours();
 
-  const { HOME } = homeScreenStr;
+  const {HOME} = homeScreenStr;
   const currentDayNumber = (new Date().getDay() + 7 - 1) % 7;
+
+  const displayOpeningHours = (dataOpeningTimes, currentDayNumber) => {
+    return Object.entries(dataOpeningTimes).map(
+      ([dayName, hoursData], index, array) => {
+        const isClosed = hoursData.length === 0;
+
+        if (isClosed) {
+          return (
+            <ClosedDay
+              key={dayName}
+              day={dayName}
+              isToday={index === currentDayNumber}
+              isClosed
+            />
+          );
+        }
+
+        let start = 0;
+        let end = hoursData.length - 1;
+        let hoursDataCopy = [...hoursData];
+
+        if (hoursData[end].type === 'open') {
+          const nextIndex = index === array.length ? 0 : index + 1;
+
+          const nextDayClosing = array[nextIndex][1][0].value;
+
+          hoursDataCopy = hoursDataCopy.concat({
+            type: 'close',
+            value: nextDayClosing,
+          });
+        }
+
+        if (hoursData[start].type === 'close') {
+          hoursDataCopy = hoursDataCopy.slice(1);
+        }
+
+        const openClosePairs = hoursDataCopy.reduce((acc, curr, i) => {
+          return i % 2 === 0
+            ? [...acc, [curr.value]]
+            : [...acc.slice(0, -1), [...acc.slice(-1)[0], curr.value]];
+        }, []);
+
+        const openingHoursStr = openClosePairs.reduce(
+          (acc, openClosePair, i) => {
+            const openTimeHours = openClosePair[0] / 60 / 60;
+            const closeTimeHours = openClosePair[1] / 60 / 60;
+
+            return `${i === 0 ? acc : `${acc}, `}${
+              openTimeHours % 12 !== 0 ? openTimeHours % 12 : openTimeHours
+            } ${openTimeHours < 12 ? 'AM' : 'PM'} - ${
+              closeTimeHours % 12 !== 0 ? closeTimeHours % 12 : closeTimeHours
+            } ${closeTimeHours < 12 ? 'AM' : 'PM'}`;
+          },
+          '',
+        );
+
+        return (
+          <OpenDay
+            key={dayName}
+            day={dayName}
+            isToday={index === currentDayNumber}
+            openingHours={openingHoursStr}
+          />
+        );
+      },
+    );
+  };
 
   if (errorOpeningTimesResponse) {
     return (
@@ -111,79 +178,7 @@ const HomeScreen = ({ }) => {
           <View style={styles.openingHours}>
             <View style={styles.openingHoursContainer}>
               <OpeningHeader />
-
-              {Object.entries(dataOpeningTimes).map(
-                ([dayName, hoursData], index, array) => {
-                  const isClosed = hoursData.length === 0;
-
-                  if (isClosed) {
-                    return (
-                      <ClosedDay
-                        key={dayName}
-                        day={dayName}
-                        isToday={index === currentDayNumber}
-                        isClosed
-                      />
-                    );
-                  }
-
-                  let start = 0;
-                  let end = hoursData.length - 1;
-                  let hoursDataCopy = [...hoursData];
-
-                  if (hoursData[end].type === 'open') {
-                    const nextIndex = index === array.length ? 0 : index + 1;
-
-                    const nextDayClosing = array[nextIndex][1][0].value;
-
-                    hoursDataCopy = hoursDataCopy.concat({
-                      type: 'close',
-                      value: nextDayClosing,
-                    });
-                  }
-
-                  if (hoursData[start].type === 'close') {
-                    hoursDataCopy = hoursDataCopy.slice(1);
-                  }
-
-                  const openClosePairs = hoursDataCopy.reduce(
-                    (acc, curr, i) => {
-                      return i % 2 === 0
-                        ? [...acc, [curr.value]]
-                        : [
-                          ...acc.slice(0, -1),
-                          [...acc.slice(-1)[0], curr.value],
-                        ];
-                    },
-                    [],
-                  );
-
-                  const openingHoursStr = openClosePairs.reduce(
-                    (acc, openClosePair, i) => {
-                      const openTimeHours = openClosePair[0] / 60 / 60;
-                      const closeTimeHours = openClosePair[1] / 60 / 60;
-
-                      return `${i === 0 ? acc : `${acc}, `}${openTimeHours % 12 !== 0
-                        ? openTimeHours % 12
-                        : openTimeHours
-                        } ${openTimeHours < 12 ? 'AM' : 'PM'} - ${closeTimeHours % 12 !== 0
-                          ? closeTimeHours % 12
-                          : closeTimeHours
-                        } ${closeTimeHours < 12 ? 'AM' : 'PM'}`;
-                    },
-                    '',
-                  );
-
-                  return (
-                    <OpenDay
-                      key={dayName}
-                      day={dayName}
-                      isToday={index === currentDayNumber}
-                      openingHours={openingHoursStr}
-                    />
-                  );
-                },
-              )}
+              <>{displayOpeningHours(dataOpeningTimes, currentDayNumber)}</>
             </View>
           </View>
         </View>
