8b9c7cb8621affc0deb56505d0d3d9e96ecf2e38
sergio junca <sergio.junca@gmail.com>
2023-01-26 15:15:27 -0500

    fix: corrected test

--- __tests__/OpenDay.test.js
+++ __tests__/OpenDay.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
-
+import homeScreenStr from '../src/constants/homeScreenStr';
 import OpenDay from '../src/components/OpenDay';
+
+const {HOME} = homeScreenStr;
 
 describe('Open Day', () => {
   test('Should render OpenDay on a Monday correctly', async () => {
@@ -13,7 +15,7 @@ describe('Open Day', () => {
   test('Should render OpenDay and Monday correctly', async () => {
     const component = render(<OpenDay isToday={true} />);
     expect(component).toBeDefined();
-    expect(component.getByText('TODAY')).toBeDefined();
+    expect(component.getByText(HOME.TODAY)).toBeDefined();
   });
 
   test('Should render OpenDay, Today, Wenesday opening hours correctly', async () => {
@@ -22,7 +24,7 @@ describe('Open Day', () => {
     );
     expect(component).toBeDefined();
     expect(component.getByText('Wenesday')).toBeDefined();
-    expect(component.getByText('TODAY')).toBeDefined();
+    expect(component.getByText(HOME.TODAY)).toBeDefined();
     expect(component.getByText('10 AM - 6pm')).toBeDefined();
   });
 });
--- __tests__/OpeningHeader.test.js
+++ __tests__/OpeningHeader.test.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import OpeningHeader from '../src/components/OpeningHeader';
 
+import homeScreenStr from '../src/constants/homeScreenStr';
+
+const {HOME} = homeScreenStr;
+
 describe('Opening Header', () => {
   test('Should render Header correctly', async () => {
     const component = render(<OpeningHeader />);
     expect(component).toBeDefined();
-    expect(component.getByText('Opening hours')).toBeDefined();
+    expect(component.getByText(HOME.OPENING_HOURS)).toBeDefined();
   });
 });
